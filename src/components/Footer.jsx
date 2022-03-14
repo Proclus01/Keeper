a82252da54e9d39b84c8f1d0307a1369430ca232
Neven <osiris@Osireion.local>
2022-03-13 20:35:24 -0700

    Update code for new challenge

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,12 @@
-import React from 'react';
-
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-
-function year() {
-    return new Date().getFullYear();
-}
+import React from "react";
 
 function Footer() {
-    return (
-        <footer>
-            <p>Copyright &copy; {year()}</p>
-        </footer>
-    );
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright &copy; {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import notes from "../notes";
-import Card from "./Card";
+import React from "react";
 
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
+//Challenge:
+//1. Implement the "add note" functionality
+//- create a constant that keeps track of the title and content
+//- Pass the new note back to the App
+//- Add new note to an array
+//- Take array and render separate Note components for each line
 
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
+//2. Implement the delete note functionality
+//- Callback from the Note component to trigger a delete function
+//- Use the filter function to filter out the item that needs deletion
+//- Pass an id over to the Note component, then
+//pass it back to the App when deleting
 
-function Note() {
-    return (
-      <div>
-        {notes.map( (noteTerm) => (
-          <Card
-            key={noteTerm.id}
-            title={noteTerm.title}
-            content={noteTerm.content}
-          />
-        ))}
-      </div>
-    );
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
+
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button>DELETE</button>
+    </div>
+  );
 }
 
 export default Note;
