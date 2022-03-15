b513ff807c9ae83e56b18564b0d7e7ab8b3f1a30
Neven <osiris@Osireion.local>
2022-03-15 02:07:56 -0700

    completed CreateArea component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,16 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  //
+
+  function areaHandler(event) {
+    console.log(event);
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={areaHandler} />
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
@@ -16,3 +22,19 @@ function App() {
 }
 
 export default App;
+
+//Challenge:
+//1. Implement the "add note" functionality
+//- create a constant that keeps track of the title and content
+//- Pass the new note back to the App
+//- Add new note to an array
+//- Take array and render separate Note components for each line
+
+//2. Implement the delete note functionality
+//- Callback from the Note component to trigger a delete function
+//- Use the filter function to filter out the item that needs deletion
+//- Pass an id over to the Note component, then
+//pass it back to the App when deleting
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// The Purpose of This Component:
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//
+// (1.) To capture two separate inputs into a single object {title, content},
+// (2.) And then store this object as state
+// (3.) And pass this state upwards to the App component
+//
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function CreateArea(props) {
+  // Define a state
+  // State is a pair of strings, the note Title and the note Content
+  const [noteState, setNoteState] = useState({
+    title: "",
+    content: ""
+  });
+
+  // Declare event handler for events from <input> and <textarea>
+  function handleInput(event) {
+    // Prevent refresh
+    event.preventDefault();
+
+    // Select events from components
+    const {name, value} = event.target;
+
+    // Update state with components
+    setNoteState( (prevItems) => {
+      return {...prevItems,
+              [name]: value}
+      }
+    );
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={handleInput} value={noteState.title} name="title" placeholder="Title" />
+        <textarea onChange={handleInput} value={noteState.content} name="content" placeholder="Take a note..." rows="3" />
+        <button
+          onClick= {
+            // Call function to pass state upwards
+            // Event handler of parent component will catch this state and store in an array
+            (event) => {
+              // Prevent refresh
+              event.preventDefault();
+
+              // Take props from CreateArea and pass noteState upwards to parent
+              props.onAdd(noteState);
+
+              // Reset input text when button is clicked
+              setNoteState(
+                {
+                  title: "",
+                  content: ""
+                }
+              );
+            }
+          }
+          >Add</button>
       </form>
     </div>
   );
